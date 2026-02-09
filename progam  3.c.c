f4fbb4b1ce878297b264a2939e062b45c417771f
meham4965-dev <meham4965@gmail.com>
2026-02-09 11:52:03 +0530

    Add files via upload

---
+++ progam  3.c.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    //Test case 1
+    int number=10;
+    printf("%d\n",number);
+    {
+        //Test case 2
+    int number=25;
+    printf("%d",number);
+
+    return 0;
+    }
+}
