29c52bd4467914a5348548e7bbbce4baeb162976
meham4965-dev <meham4965@gmail.com>
2026-02-10 13:29:17 +0530

    Add files via upload

---
+++ using -.c.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int a=8;
+    int b=3;
+    int c=10;
+    int d=5;
+    int e=15;
+    int f=10;
+    
+    printf("%d\n",a-b);
+    printf("%d\n",c-d);
+    printf("%d",e-f);
+
+    return 0;
+}
