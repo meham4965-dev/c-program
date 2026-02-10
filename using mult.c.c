541fba533fbe28107fa39906e4c10962eacb9f38
meham4965-dev <meham4965@gmail.com>
2026-02-10 13:32:44 +0530

    Add files via upload

---
+++ using mult.c.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int a=4;
+    int b=5;
+    int c=10;
+    int d=2;
+    int e=1;
+    int f=20;
+    
+    printf("%d\n",a*b);
+    printf("%d\n",c*d);
+    printf("%d",e*f);
+
+    return 0;
+}
