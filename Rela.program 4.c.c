55325cdc50f0ce8bb56ceb84c06c186e1be496e8
meham4965-dev <meham4965@gmail.com>
2026-02-11 10:55:48 +0530

    Add files via upload

---
+++ Rela.program 4.c.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=5;
+    int b=3;
+    int c=4;
+    int d=4;
+    int e=9;
+    int f=1;
+    printf("\n%d",a!=b);
+    printf("\n%d",c!=d);
+    printf("\n%d",e!=f);
+    return 0;
+}
