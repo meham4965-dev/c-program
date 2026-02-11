e56877657b550d45df2554e3495e8dd1563c38c7
meham4965-dev <meham4965@gmail.com>
2026-02-11 14:21:23 +0530

    Add files via upload

---
+++ ass.prog 3.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    int b=5;
+    int c=20;
+    int d=10;
+    int e=5;
+    int f=5;
+    printf("%d",a-=b);
+    printf("\n%d",c-=d);
+    printf("\n%d",e-=f);
+    return 0;
+}
