c42777ba06978d4a85cfd4ad57c29b98b994763c
meham4965-dev <meham4965@gmail.com>
2026-02-11 10:34:23 +0530

    Add files via upload

---
+++ decrement variable twice.c.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=6;
+    int b=2;
+    int c=10;
+    printf("\n%d",--a,--a);
+    printf("\n%d",--b,--b);
+    printf("\n%d",--c,--c);
+    return 0;
+}
