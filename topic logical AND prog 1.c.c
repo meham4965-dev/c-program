1783a0febb1c9f70099dedff045c305f9d2ea987
meham4965-dev <meham4965@gmail.com>
2026-02-11 11:22:51 +0530

    Add files via upload

---
+++ topic logical AND prog 1.c.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=5;
+    int b=3;
+    int c=1;
+    int d=1;
+    int e=0;
+    int f=5;
+    printf("%d",a&&b,a||b);
+    printf("\n%d",c&&d,c||d);
+    printf("\n%d",e&&f,e||f);
+    return 0;
+}
