338d055d3052bff227fbaf07f5ec4ee03c949d7a
meham4965-dev <meham4965@gmail.com>
2026-02-11 10:40:30 +0530

    Add files via upload

---
+++ topic relational program 1.c.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    int b=5;
+    int c=3;
+    int d=8;
+    int e=7;
+    int f=7;
+    printf("\n%d",a>b);
+    printf("\n%d",c>d);
+    printf("\n%d",e>f);
+    return 0;
+}
