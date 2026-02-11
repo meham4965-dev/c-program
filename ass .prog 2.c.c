b1d08552cbb2a5f9d3d1e85e53649506b7a72527
meham4965-dev <meham4965@gmail.com>
2026-02-11 14:15:50 +0530

    Add files via upload

---
+++ ass .prog 2.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    int b=5;
+    int c=3;
+    int d=7;
+    int e=0;
+    int f=4;
+    printf("%d",a+=b);
+    printf("\n%d",c+=d);
+    printf("\n%d",e+=f);
+    return 0;
+}
