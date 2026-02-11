2c4107a20befcd2bed006112b7bb9fde8b09563e
meham4965-dev <meham4965@gmail.com>
2026-02-11 13:38:26 +0530

    Add files via upload

---
+++ logic or prog 2.c.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=0;
+    int b=5;
+    int c=0;
+    int d=0;
+    int e=3;
+    int f=7;
+    printf("%d",a||b);
+    printf("\n%d",c||d);
+    printf("\n%d",e||f);
+    return 0;
+}
