75ba22846ba2cd5a1b7e7da203942b973992553f
meham4965-dev <meham4965@gmail.com>
2026-02-11 15:54:09 +0530

    Add files via upload

---
+++ if prog .5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    if(a%4==0){
+        printf("Leap year");
+    }
+    else{
+        printf("not a leap year");
+        return 0;
+    }
+}
