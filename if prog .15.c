e14376d8fc4d7b38dea10ad285b946858c695a41
meham4965-dev <meham4965@gmail.com>
2026-02-12 14:09:07 +0530

    Add files via upload

---
+++ if prog .15.c
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b+c==180){
+    printf("valid tringle");
+    }
+    else{
+        printf("not valid tringle");
+    }
+
+    return 0;
+}
