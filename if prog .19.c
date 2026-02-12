d6c1bd2e541960f0213c618161e41e84c861990e
meham4965-dev <meham4965@gmail.com>
2026-02-12 15:30:39 +0530

    Add files via upload

---
+++ if prog .19.c
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>0-9){
+    printf("digit");
+    }
+    else{
+        printf("not digit");
+    }
+
+    return 0;
+}
