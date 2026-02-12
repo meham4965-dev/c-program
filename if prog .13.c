21497c52647c7666d7d75d6ada613dbcccc5ea89
meham4965-dev <meham4965@gmail.com>
2026-02-12 14:00:46 +0530

    Add files via upload

---
+++ if prog .13.c
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=65&& a>=97){
+    printf("uppercase letter");
+    }
+    else{
+        printf("not lowercase letter");
+    }
+
+    return 0;
+}
