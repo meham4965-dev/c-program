53722fe67ba3116a7efba60cd19855cdf2b599ff
meham4965-dev <meham4965@gmail.com>
2026-02-12 12:16:28 +0530

    Add files via upload

---
+++ if prog .10.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    if("a>=100,b>=999"){
+       printf("three-digit number");
+    }
+    
+    else{
+       printf("not three-digit number");
+    }
+
+    return 0;
+}
