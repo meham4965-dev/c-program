483dabb3a67844057c65624e7f3359a6a365370a
meham4965-dev <meham4965@gmail.com>
2026-02-12 11:53:41 +0530

    Add files via upload

---
+++ if prog .9.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if("Age>=18"){
+       printf("eligible to vote");
+    }
+    
+    else{
+       printf("not eligible to vote");
+    }
+
+    return 0;
+}
