2ce5c34e07111265e1022a08823ca01346ec749b
meham4965-dev <meham4965@gmail.com>
2026-02-12 13:53:44 +0530

    Add files via upload

---
+++ if prog .12.c
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a,&b);
+    if(a>=75,b<90){
+    printf("Grade B");
+    }
+    else{
+        printf("Grade not B");
+    }
+
+    return 0;
+}
