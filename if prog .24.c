906a6ada2be92c3df80cfe2d9a6a654c41859788
meham4965-dev <meham4965@gmail.com>
2026-02-13 11:35:25 +0530

    Add files via upload

---
+++ if prog .24.c
@@ -0,0 +1,31 @@
+  
+#include <stdio.h>
+
+int main()
+{
+    
+    int vowel;
+    scanf("%d",&vowel);
+    switch(vowel){
+        case'A':
+        printf("vowel");
+        break;
+        case'E':
+        printf("vowel");
+        break;
+        case'I':
+        printf("vowel");
+        break;
+        case'O':
+        printf("vowel");
+        break;
+        case'U':
+        printf("vowel");
+        break;
+        
+    default:
+    printf("consonant");
+    }
+
+    return 0;
+}
